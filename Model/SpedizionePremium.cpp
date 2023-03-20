f94f860a9e08934a05cf2e97e003382f8caab3a3
dugoalberto <www.da.0502@gmail.com>
2023-03-20 21:03:27 +0100

    setOrario

--- Model/SpedizionePremium.cpp
+++ Model/SpedizionePremium.cpp
@@ -6,12 +6,11 @@
 
 SpedizionePremium::SpedizionePremium(int trakingNumber, const Address &mittente, const Address &destinatario,
                                      const Package &pacco, int peso, double volume, const Stato &stato,
-                                     const string &descrizione, double costo, int year, int month, int day, int hours, int minutes)
+                                     const string &descrizione, double costo, int anno, int mese, int giorni, int ore, int minuti)
                                      :Spedizione(trakingNumber, mittente, destinatario, pacco, peso, volume, stato, descrizione, costo) {
-    /*orario_tm.tm_year = year - 1900;
-    orario_tm.tm_mon = month - 1;
-    orario_tm.tm_mday = day;
-    orario_tm.tm_hour = hours;
-    orario_tm.tm_min = minutes;*/
-
+    arrivo_tm.tm_year = anno - 1900;
+    arrivo_tm.tm_mon = mese - 1;
+    arrivo_tm.tm_mday = giorni;
+    arrivo_tm.tm_hour = ore;
+    arrivo_tm.tm_min = minuti;
 }
--- Model/SpedizionePremium.h
+++ Model/SpedizionePremium.h
@@ -11,16 +11,25 @@
 class SpedizionePremium: public Spedizione{
 private:
     // ottieni l'orario corrente
-    /*
-    std::time_t now = std::time(nullptr); //utilizzato per vedere in quanto tempo arriva il pacco
-    std::tm now_tm = *std::localtime(&now);
-    std::time_t orario = std::time(nullptr);
-    std::tm orario_tm = *std::localtime(&orario);*/
+    std::time_t ordine = std::time(nullptr); //utilizzato per vedere in quanto tempo arriva il pacco, ordine è l'orario nel quale è stato ordinata la spedizione
+    std::tm ordine_tm = *std::localtime(&ordine);
+    std::time_t arrivo = std::time(nullptr); //orario è in secondi dall'epoch
+    std::tm arrivo_tm = *std::localtime(&arrivo);
+
 public:
     SpedizionePremium(int trakingNumber, const Address &mittente, const Address &destinatario, const Package &pacco,
-                      int peso, double volume, const Stato &stato, const string &descrizione, double costo, int year, int month, int day, int hours, int minutes);
+                      int peso, double volume, const Stato &stato, const string &descrizione, double costo,
+                      int anno, int mese, int giorni, int ore, int minuti);
 
-
+    std::tm setOrario(int year, int month, int day, int hours, int minuti){
+        std::time_t tday = std::time(nullptr); //orario di oggi, mi serve per vedere se cambia l'orario e lo mette nel passato
+        if( year < ordine_tm.tm_year || month < ordine_tm.tm_mon ||
+            ( month == ordine_tm.tm_mon &&  day < ordine_tm.tm_mday) ) throw;
+        this->arrivo_tm.tm_year = year;
+        this->arrivo_tm.tm_mon = month;
+        this->arrivo_tm.tm_mday= day;
+        this->arrivo_tm.tm_min = minuti;
+    }
 };
 
 #endif //PROJECT_SPEDIZIONEPREMIUM_H
